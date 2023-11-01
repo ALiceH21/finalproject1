784022fdee7e806ce0f96d8ee540bc89ce841585
Alice <144723624+ALiceH21@users.noreply.github.com>
2023-11-01 13:42:03 -0400

    Add files via upload

---
+++ Characters.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Characters
+{
+  public Scanner askForCharacter()
+  {
+   System.out.println("enter the name of a character or person");
+   Scanner n = new Scanner(System.in);
+   return n;
+  }
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+  public static void main(String[] args)
+  {
+    Characters ch = new Characters();
+    ch.askForCharacter();
+  }
+}
+
